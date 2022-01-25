3fb8fc709421a47d41f17cdec089833a4e3a96ee
Ghanshyam Dutt <ghanshyamdutt1407@gmail.com>
2022-01-25 20:00:59 +0530

    activities section ---> create post

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -52,7 +52,24 @@
                     <div class="font-semibold"> today .</div>
                   </div>
               </div>
-              
+              <!-- Activities Section -->
+             
+              <div class="activities flex justify-end py-3 px-2">
+                <div class="flex space-x-2">
+                    <div class="font-semibold">I am  </div>
+                    <div class="">
+                      <input type="text" list="activities" placeholder="Start typing ..." class="border border-gray-200 pl-2 rounded-lg" />
+                      <datalist id="activities">
+                        <option value="Watching a movie"></option>
+                        <option value="Snoring"></option>
+                        <option value="Cooking"></option>
+                        <option value="Dancing"></option>
+                        <option value="Cycling"></option>
+                      </datalist>
+                    </div>
+                    <div class="font-semibold"> today .</div>
+                </div>
+              </div>
               
               <!-- Photos Section -->
               <div class="photos flex space-x-2 py-3 px-2">
